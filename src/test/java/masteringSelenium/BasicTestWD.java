14bdb972cfa878f0d8317c60ab7090b773183bd1
Mahajan <vmahajan@ms.ds.uhc.com>
2018-01-16 08:45:30 +0530

    Commiting Driver Factory & WebDriverThread & BaseTestWD

---
+++ src/test/java/masteringSelenium/BasicTestWD.java
@@ -0,0 +1,60 @@
+package masteringSelenium;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasicTestWD extends DriverFactory {
+	
+	private void googleExampleThatSearchFor(final String searchString) throws Exception
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\vmahajan\\Downloads\\Day1\\Browser_Drivers_32bit\\chromedriver.exe");
+		WebDriver driver=DriverFactory.getDriver();
+		driver.get("https://www.google.com");
+		WebElement searchField =driver.findElement(By.name("q"));
+		searchField.clear();
+		searchField.sendKeys(searchString);
+		System.out.println("Titel is" + driver.getTitle());
+		searchField.submit();
+		(new WebDriverWait(driver,10)).until
+				(new ExpectedCondition<Boolean>()
+				{
+						public Boolean apply(WebDriver driverObject)
+							{
+								return driverObject.getTitle().toLowerCase().startsWith(searchString.toLowerCase());
+							}
+					}
+				);
+		
+		System.out.println("Titel is" + driver.getTitle());
+	}
+	
+	@Test
+	public void googleCheeseExample() throws Exception
+	{
+		googleExampleThatSearchFor("Cheese");
+		
+	}
+	@Test
+	public void googleMilkExample() throws Exception
+	{
+		googleExampleThatSearchFor("Milk");
+		
+	}
+	@Test
+	public void googleCheeseExample1() throws Exception
+	{
+		googleExampleThatSearchFor("Cheese1");
+		
+	}
+	@Test
+	public void googleMilkExample1() throws Exception
+	{
+		googleExampleThatSearchFor("Milk1");
+		
+	}
+
+}
---
+++ src/test/java/masteringSelenium/DriverFactory.java
@@ -0,0 +1,37 @@
+package masteringSelenium;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeSuite;
+
+public class DriverFactory {
+	
+	private static ThreadLocal<WebDriverThread> driverThread;
+	@BeforeSuite
+	public static void instantiateDriverObject()
+	{
+		driverThread=new ThreadLocal<WebDriverThread>()
+				{
+					@Override
+					protected WebDriverThread initialValue()
+					{
+						WebDriverThread webDriverThread=new WebDriverThread();
+						return webDriverThread;
+					}
+				
+				};
+	}
+
+	public static WebDriver getDriver() throws Exception 
+	{
+		return driverThread.get().getDriver();
+		
+	}
+	
+	@AfterMethod
+	public static void quitDriver()
+	{
+		driverThread.get().quitDriver();
+	}
+	
+}
