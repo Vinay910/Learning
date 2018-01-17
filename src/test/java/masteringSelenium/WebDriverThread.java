ee970b20e85fbeab9878d9552fcf213710047ff1
vmahajan <vmahajan@LH7U0CNU408BXHV.ms.ds.uhc.com>
2018-01-17 12:30:12 +0530

    Added Interface and Enum

--- src/test/java/masteringSelenium/WebDriverThread.java
+++ src/test/java/masteringSelenium/WebDriverThread.java
@@ -4,10 +4,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import config.DriverType;
+
+import static config.DriverType.FIREFOX;
+import static config.DriverType.valueOf;
+
 public class WebDriverThread {
 	
 	private WebDriver webdriver;
+	private DriverType selectDriverType;
 	
+	private final DriverType defaultDriverType= FIREFOX ;
+	private final String browser=System.getProperty("browser").toUpperCase();
 	private final String operatingSystem=System.getProperty("os.name").toUpperCase();
 	private final String operatingArchitecture=System.getProperty("os.arch");
 	
@@ -15,17 +23,32 @@ public class WebDriverThread {
 	{
 		if(null==webdriver)
 		{
-			System.out.println(" ");
-			System.out.println("Current Operating System:  " + operatingSystem);
-			System.out.println("Current Architecture:  " + operatingArchitecture);
-			System.out.println("Current Browser Selection : Chrome");
-			System.out.println(" ");
-			webdriver = new ChromeDriver(DesiredCapabilities.chrome());
+			selectDriverType=determineEffectiveDriverType();
+			DesiredCapabilities desiredCapabilities=selectDriverType.getDesiredCapabilities();
+			
 		}
 		
 		return webdriver;
 	}
 	
+	private DriverType determineEffectiveDriverType() {
+		
+		DriverType driverType=defaultDriverType;
+		try
+		{
+			driverType=valueOf(browser);
+		}
+		catch(NullPointerException ignored)
+		{
+			System.out.println("No driver specified, defaulting to  " +driverType+"....");
+		}
+		catch(IllegalArgumentException ignored)
+		{
+			System.out.println("No driver specified, defaulting to  " +driverType+"....");
+		}
+		return driverType;
+	}
+
 	public void quitDriver()
 	{
 		if(null!=webdriver)
